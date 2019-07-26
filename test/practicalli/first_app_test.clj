64fb8ea378f0f12ba63e7e79ee1908aa6ff9d58f
John Stevenson <john@jr0cket.co.uk>
2019-07-26 18:28:05 +0100

    New: created project using Clojure CLI tools
    clj -A:new app practicalli/first-app

---
+++ test/practicalli/first_app_test.clj
@@ -0,0 +1,7 @@
+(ns practicalli.first-app-test
+  (:require [clojure.test :refer :all]
+            [practicalli.first-app :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
